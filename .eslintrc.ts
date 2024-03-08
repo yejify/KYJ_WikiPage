af378a0823454b386139ec6fa944246f59f43a14
yeji <skql4128@naver.com>
2024-03-09 03:03:26 +0900

    chore: 패키지 매니저 수정, prettier,eslint 설정

---
+++ .eslintrc.ts
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'react-app',
+    'prettier',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'no-unused-vars': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+};
