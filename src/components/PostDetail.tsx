c9d8618c02ae838afe32f440e8ee0cee247cf3b1
yeji <skql4128@naver.com>
2024-03-09 03:05:43 +0900

    fix: 내용 안에 제목이 여러개일 때 링크가 하나만 생성되는 오류 수정

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
+import NotFound from './NotFound';
+import { styled } from 'styled-components';
 
 interface Post {
   id: number;
@@ -20,35 +22,50 @@ function PostDetail({ posts, onDelete }: PostDetailProps) {
   const post = posts.find((post) => post.id.toString() === postId);
 
   if (!post) {
-    return <p>포스트를 찾을 수 없습니다.</p>;
+    return <NotFound />;
   }
 
-  const sortedPosts = [...posts].sort(
-    (a, b) => b.title.length - a.title.length
-  );
+  // 전체 내용을 담을 배열
+  const contentWithLinks: React.ReactNode[] = [];
+  let lastIndex = 0; // 마지막으로 처리한 인덱스
 
-  const contentComponents: React.ReactNode[] = [];
-  let remainingContent = post.content;
-
-  sortedPosts.forEach(({ id, title }) => {
-    const index = remainingContent.indexOf(title);
-    if (index !== -1) {
-      if (index > 0) {
-        contentComponents.push(remainingContent.substring(0, index));
+  // 제목이 포함된 위치를 찾아서 링크를 생성하는 함수
+  posts.forEach(({ id, title }) => {
+    let titleIndex = post.content.indexOf(title, lastIndex);
+    while (titleIndex !== -1) {
+      // 제목 이전의 텍스트 추가
+      if (titleIndex > lastIndex) {
+        contentWithLinks.push(post.content.substring(lastIndex, titleIndex));
       }
 
-      contentComponents.push(
-        <Link key={id} to={`/postDetail/${id}`}>
+      // 제목을 Link로 변환
+      contentWithLinks.push(
+        <Link key={`${id}-${titleIndex}`} to={`/postDetail/${id}`}>
           {title}
-        </Link>
+        </Link>,
       );
 
-      remainingContent = remainingContent.substring(index + title.length);
+      // 조사 처리: 제목 바로 뒤의 조사를 식별하기 위한 정규식
+      const postTitleRegex = new RegExp(`^(${title})([가-힣]{1,2})`, 'g');
+      const matches = postTitleRegex.exec(post.content.substring(titleIndex));
+
+      if (matches && matches[2]) {
+        // 조사가 식별된 경우, 조사도 텍스트로 추가
+        contentWithLinks.push(matches[2]);
+      }
+
+      // 다음 검색 시작 위치 업데이트
+      lastIndex =
+        titleIndex +
+        title.length +
+        (matches && matches[2] ? matches[2].length : 0);
+      titleIndex = post.content.indexOf(title, lastIndex); // 다음 제목 위치 검색
     }
   });
 
-  if (remainingContent) {
-    contentComponents.push(remainingContent);
+  // 마지막 제목 이후의 내용 추가
+  if (lastIndex < post.content.length) {
+    contentWithLinks.push(post.content.substring(lastIndex));
   }
 
   const handleEditClick = () => {
@@ -61,13 +78,67 @@ function PostDetail({ posts, onDelete }: PostDetailProps) {
   };
 
   return (
-    <>
-      <h2>{post.title}</h2>
-      <div>{contentComponents}</div>{' '}
-      <button onClick={handleEditClick}>수정</button>
-      <button onClick={handleDelete}>삭제</button>
-    </>
+    <PostDetailWrap>
+      <h2>
+        TITLE | <span>{post.title}</span>
+      </h2>
+      <p id='contentTitle'>CONTENT</p>
+      <div id='contentTxt'>{contentWithLinks}</div>
+      <div id='buttonBox'>
+        <button onClick={handleEditClick}>EDIT</button>
+        <button onClick={handleDelete}>DELETE</button>
+      </div>
+    </PostDetailWrap>
   );
 }
 
 export default PostDetail;
+
+const PostDetailWrap = styled.section`
+  margin: 30px 100px 0 100px;
+  width: auto;
+  h2 {
+    font-size: 40px;
+    margin-bottom: 30px;
+  }
+  h2 span {
+    font-size: 35px;
+    color: #7d7b7b;
+  }
+  #contentTitle {
+    margin-bottom: 16px;
+    font-size: 38px;
+  }
+  button {
+    border: 1px solid;
+    border-radius: 5px;
+    width: 150px;
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
+    font-size: 20px;
+    &:hover {
+      color: #fff;
+      background-color: #000;
+    }
+  }
+  #contentTxt {
+    min-height: 250px;
+    border-radius: 10px;
+    background-color: #f6f6f6;
+    padding: 20px;
+    margin-bottom: 30px;
+    color: #7d7b7b;
+    line-height: 1.5;
+  }
+  #contentTxt a {
+    color: #000;
+    text-decoration: underline;
+  }
+  #buttonBox {
+    display: flex;
+    gap: 25px;
+    position: absolute;
+    right: 100px;
+  }
+`;
